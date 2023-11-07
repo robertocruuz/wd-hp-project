a6e3af3f434a6c48a3c737cdb34b568b3b05b253
Roberto Cruz <cruzrondan@gmail.com>
2023-11-06 21:21:35 -0300

    Maps

--- code.js
+++ code.js
@@ -6,3 +6,17 @@ function alertaEnvio() {
 }
 
 btEnviar.onclick = () => alertaEnvio();
+
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { 'lat: -34.397, lng: 150.644' },
+    disableDefaultUI: true,
+    zoom: 8,
+  });
+}
+
+initMap();
